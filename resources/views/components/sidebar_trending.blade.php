991d796600e5ad0c515b523d2425cd9c8af3f84a
NEC <NEC@DESKTOP-1FI84QG>
2020-10-27 09:50:21 +0900

    初期サンプル

---
+++ resources/views/components/sidebar_trending.blade.php
@@ -0,0 +1,18 @@
+				<article id="todayRank" class="block ranking">
+					<header>
+						<h1>Trending Community</h1>
+					</header>
+					<div>
+						<ol>
+							<li>r/XXXXX</li>
+							<li>r/XXXXX</li>
+							<li>r/XXXXX</li>
+							<li>r/XXXXX</li>
+						</ol>
+					</div>
+					<div>
+						<input type="button" value="VIEW ALL" class="btn btn-primary">
+						<div>
+						</div>
+					</div>
+				</article>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('top');
+});
+Route::get('/submit', function () {
+    return view('submit');
 });
