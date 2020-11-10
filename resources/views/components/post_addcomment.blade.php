cf569200b75dbca2b03b837aa31555438ad4389c
MSG\1817002 <wasabinoriparipari@gmail.com>
2020-11-10 11:39:06 +0900

    no message

---
+++ resources/views/components/post_addcomment.blade.php
@@ -0,0 +1,13 @@
+<div class="add-comment-field">
+    <form action="#">
+    <div class="mdl-textfield mdl-js-textfield">
+        <textarea class="mdl-textfield__input" type="text" rows="3" id="sample5"></textarea>
+        <label class="mdl-textfield__label" for="sample5">Ur comment...</label>
+    </div>
+    </form>
+    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
+    POST
+    </button>
+    <div class="add-comment-field-border">
+    </div>
+</div>
