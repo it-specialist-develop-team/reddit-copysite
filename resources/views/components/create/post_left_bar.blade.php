de950c958a407dea2fa5c7d622c0b357a77918e9
DESKTOP-UO1TVUF\popco <msg.1801026@gmail.com>
2020-11-16 20:06:30 +0900

    復旧作業

--- resources/views/components/create/post_left_bar.blade.php
+++ resources/views/components/create/post_left_bar.blade.php
@@ -6,8 +6,8 @@
 
     <div class="mdl-tabs mdl-js-tabs mdl-js-ripple-effect">
       <div class="mdl-tabs__tab-bar">
-        <a href="#starks-panel" class="mdl-tabs__tab is-active" id="post_select">POST</a>
-        <a href="#lannisters-panel" class="mdl-tabs__tab" id="image_video_select">IMAGE & VIDEO</a>
+        <a href="#starks-panel" class="mdl-tabs__tab is-active">POST</a>
+        <a href="#lannisters-panel" class="mdl-tabs__tab">IMAGE & VIDEO</a>
       </div>
 
       <!-- POST -->
@@ -16,12 +16,14 @@
 
           <form action="#">
             <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-              <input class="mdl-textfield__input" type="text" id="title">
+              <input class="mdl-textfield__input" type="text" id="sample3">
               <label class="mdl-textfield__label" for="sample3">Title...</label>
             </div>
           </form>
+
           <div id="summernote">
           </div>
+
           <script>
             $(document).ready(function () {
               $('#summernote').summernote();
@@ -64,6 +66,8 @@
 
         </div>
 
+
+
       </div>
       <!-- Image n Video -->
       <div class="mdl-tabs__panel" id="lannisters-panel">
@@ -79,7 +83,9 @@
           <div class="media-drop-field">
             <div class="dragndrop">
               <span>Drag and Drop or </span>
-              <input type="file" name="image" class="mdl-button mdl-js-button mdl-js-ripple-effect">
+              <button class="mdl-button mdl-js-button mdl-js-ripple-effect">
+                Button
+              </button>
 
             </div>
           </div>
@@ -152,15 +158,23 @@
         </div>
 
         <div class="submit-button-field">
-          <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
+        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
             SAVE
           </button>
-          <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" id="post_btn">
+          <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
             POST
           </button>
+
         </div>
+
+
       </div>
+
+
     </div>
+
+
+
   </div>
 
 </div>
--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,104 +1,62 @@
 @extends('layouts.base')
 @php
-$title="Create Post";
+  $title="Create Post";
 @endphp
 
 @section('header-links')
-<link rel="stylesheet" href="css/reader-board.css">
+  <link rel="stylesheet" href="css/reader-board.css">
 
-<!-- Summernote -->
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
-{{-- <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> --}}
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
-<link rel="stylesheet" href="css/create.css">
+  <!-- Summernote -->
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+  <link rel="stylesheet" href="css/create.css">
 
 @endsection
 
 
 @section('content')
 
-<div class="page-content">
+      <div class="page-content">
 
-  <!-- SUBHEADER -->
-  <div class="sub-header-readerboard">
-    <h5>新規投稿</h5>
-    <span>subredditに対して新しい投稿を作成します。</span>
-  </div>
+        <!-- SUBHEADER -->
+        <div class="sub-header-readerboard">
+          <h5>新規投稿</h5>
+          <span>subredditに対して新しい投稿を作成します。</span>
+        </div>
 
-  <div class="mdl-tabs mdl-js-tabs mdl-js-ripple-effect">
+        <div class="mdl-tabs mdl-js-tabs mdl-js-ripple-effect">
 
-    <!-- POSTBAR// -->
-    <div class="mdl-grid">
+          <!-- POSTBAR// -->
+          <div class="mdl-grid">
 
-      <!-- LEFT BAR// -->
-      @include('components.create.post_left_bar')
+            <!-- LEFT BAR// -->
+            @include('components.create.post_left_bar')
 
-      <!-- //LEFT BAR -->
+            <!-- //LEFT BAR -->
 
-      <!-- RIGHT BAR// -->
-      <div class="mdl-cell mdl-cell--4-col">
-        <!-- RIGHT CARD#1 -->
-        @include('components.create.top_communities')
-        <!-- RIGHT CARD#2 -->
-        @include('components.create.top_communities')
-        <!-- RIGHT CARD#3 -->
-        @include('components.create.top_communities')
+            <!-- RIGHT BAR// -->
+            <div class="mdl-cell mdl-cell--4-col">
+              <!-- RIGHT CARD#1 -->
+              @include('components.create.top_communities')
+              <!-- RIGHT CARD#2 -->
+              @include('components.create.top_communities')
+              <!-- RIGHT CARD#3 -->
+              @include('components.create.top_communities')
+            </div>
+            <!-- //RIGHT BAR -->
+
+          </div>
+          <!-- //POSTBAR -->
+
+        </div>
+        <!-- //SUB HEADER -->
+
+
+
+
+
       </div>
-      <!-- //RIGHT BAR -->
-
-    </div>
-    <!-- //POSTBAR -->
-
-  </div>
-  <!-- //SUB HEADER -->
-</div>
-{{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> --}}
-<script>
-  $(function(){
-  let post_type = 0;
-  let post_select = document.getElementById("post_select");
-  let image_video_select = document.getElementById("image_video_select");
-  post_select.addEventListener("click",e=>{
-    post_type = 0;
-  });
-  image_video_select.addEventListener("click",e=>{
-    post_type = 1;
-  });
-  $('#post_btn').on("click",function(){
-    let $title = $("#title");
-    let $body = $("#summernote").summernote("code");
-    let $image = $('input[name="image"]');
-    
-    let fd = new FormData();
-    fd.append("image", $image.prop('files')[0]);
-    fd.append("body", $body);
-    fd.append("post_type", post_type);
-    fd.append("title", $title.prop('value'));
-    
-    // fd.append("id",id);
-
-    $.ajax({
-    headers: {
-    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-    },//Headersを書き忘れるとエラーになる
-    url: '/create/create_post',//ご自身のweb.phpのURLに合わせる
-    type: 'POST',//リクエストタイプ
-    contentType: false,
-    processData: false,
-    data: fd,
-    })
-    // Ajaxリクエスト成功時の処理
-    .done(function(data) {
-    
-    })
-    // Ajaxリクエスト失敗時の処理
-    .fail(function(data) {
-    alert('Ajaxリクエスト失敗');
-    });
-  });
-});
-</script>
 @endsection
