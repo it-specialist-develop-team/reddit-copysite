147664ec0d46fdf8d10ef78d6887d48d4c852076
Ikuto Harano <msg1801022@gmail.com>
2020-11-19 13:46:13 +0900

    コンポーネントの細分化

---
+++ resources/views/components/child_comment.blade.php
@@ -0,0 +1,17 @@
+<div class="comment-header">
+  <img src="img/g2-logo.jpg" class="comment-user-icon">
+  <div class="comment-header-status">
+    <span class="comment-user">u/USER</span>
+    <span class="comment-time">2 hours ago</span>
+  </div>
+</div>
+<div class="comment-body">
+  <span class="comment-body-text">sample comment sample comment sample comment</span>
+  <div class="com-icoon">
+    <p class="rep-rank-classic">↑ 00 ↓</p>
+    <a class="rep-res" href="#"><span class="material-icons quickreply">quickreply</span>Reply</a>
+    <a class="rep-share" href="#">Share</a>
+    <a class="rep-repo" href="#">Report</a>
+    <a class="rep-save" href="#">Save</a>
+  </div>
+</div>
