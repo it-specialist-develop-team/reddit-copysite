d0f7917ea3904551fe02642049c6bcad7fc8d30a
DESKTOP-UO1TVUF\popco <msg.1801026@gmail.com>
2020-11-26 12:19:18 +0900

    post修正

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,18 +7,18 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
-    // public function classic()
-    // {
-    //     $posts = Post::get();
-    //     return view('subreddit',['posts' => $posts]);
-    // }
-    public function classic($id)
-    {
-        $posts = Post::paginate(6);
-        return view('subreddit',['posts' => $posts, 'id'=>$id]) ;
-    }
-//   function show(Post $post)
-//   {
-//     return view("post", ["post" => $post]);
-//   }
+  // public function classic()
+  // {
+  //     $posts = Post::get();
+  //     return view('subreddit',['posts' => $posts]);
+  // }
+  public function classic($id)
+  {
+    $posts = Post::paginate(6);
+    return view('subreddit', ['posts' => $posts, 'id' => $id]);
+  }
+  function show(Post $post)
+  {
+    return view("post", ["post" => $post]);
+  }
 }
