10277676eb90354fbd62ad6509c37ec576e79482
MSG\1817002 <wasabinoriparipari@gmail.com>
2020-11-26 11:53:37 +0900

    no message

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -22,10 +22,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>{{$title}}</title>
-
-  <script src="{{ asset('js/jquery.min.js') }}"></script>
-  <script src="{{ asset('js/infinite-scroll.pkgd.min.js') }}"></script>
-
 </head>
 
 <body>
