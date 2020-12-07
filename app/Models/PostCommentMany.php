70b8c043b8cdd2d1065b9c218bba2178b848b0cf
masato <msg.1817004@gmail.com>
2020-12-07 15:39:54 +0900

    readerboardの機能追加

---
+++ app/Models/PostCommentMany.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class PostCommentMany extends Model
+{
+    use HasFactory;
+
+    public function getPostsByRegComment(){
+
+        $query = DB::table('posts as parent');
+        $data = $query->select(
+            'parent.id',
+            'parent.title',
+            DB::raw('(select count(*) from
+            posts as child
+            where child.parent_id = parent.id
+            ) as cnt')
+        )
+        ->whereRaw(
+            'parent.parent_id is null'
+        )
+        ->orderBy(
+            'cnt',
+            'desc',
+        )->get();
+// echo $query->toSql();
+// die('-----');
+        return $data;
+    }
+}
