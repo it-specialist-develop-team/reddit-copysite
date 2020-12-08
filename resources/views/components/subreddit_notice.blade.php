99230c1c6eee5d56f6ca97e3ce2b90d6be0e26e4
DESKTOP-UO1TVUF\popco <msg.1801026@gmail.com>
2020-12-08 11:56:55 +0900

    間違えてたので修正

--- resources/views/components/subreddit_notice.blade.php
+++ resources/views/components/subreddit_notice.blade.php
@@ -1,8 +1,7 @@
 <div class="mdl-card mdl-shadow--2dp maincard">
-
-    {{-- <p>UPDATES FROM REDDIT</p>
+    {{-- 
+    <p>UPDATES FROM REDDIT</p>
     <p>Keep yourself safe and informed</p>
     <p>Visit r/Coronavirus to talk about COVID-19, and visit www.who.int for more information.</p> --}}
-
-    <p>{!!nl2br$body!!}</p>
+    {!! nl2br($body) !!}
 </div>
